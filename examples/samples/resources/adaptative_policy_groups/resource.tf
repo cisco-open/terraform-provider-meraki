362bdc122563ee68ceacccb2d414dcdfe442ad06
Francisco Muñoz <fmunoz@cloverhound.com>
2024-05-22 15:50:11 -0600

    ## 0.2.1-alpha (April 16, 2024) BUGFIXES: * ListAttribute replaced to SetAtrribute. * NetworksWirelessSSIDsResource attributte marked as `Computed` removed cause were only `Optional` params.

---
+++ examples/samples/resources/adaptative_policy_groups/resource.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    meraki = {
+      version = "0.2.0-alpha"
+      source  = "hashicorp.com/edu/meraki"
+      # "hashicorp.com/edu/meraki" is the local built source, change to "cisco-en-programmability/meraki" to use downloaded version from registry
+    }
+  }
+}
+
+provider "meraki" {
+  meraki_debug = "true"
+}
+
+resource "meraki_organizations_adaptive_policy_groups" "repro" {
+  organization_id = "828099381482762270"
+  description     = "A repro"
+  name            = "pulumi testing 2"
+  sgt             = 1006
+}

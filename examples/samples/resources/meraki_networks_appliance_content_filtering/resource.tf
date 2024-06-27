0d326bdfb670612f1d7574861b37aa47de282960
Francisco Muñoz <fmunoz@cloverhound.com>
2024-06-27 11:13:24 -0600

    error on meraki_networks_appliance_content_filtering │ Path: blocked_url_categories_rs #82

---
+++ examples/samples/resources/meraki_networks_appliance_content_filtering/resource.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    meraki = {
+      version = "0.2.4-alpha"
+      source  = "hashicorp.com/edu/meraki"
+      # "hashicorp.com/edu/meraki" is the local built source, change to "cisco-en-programmability/meraki" to use downloaded version from registry
+    }
+  }
+}
+
+provider "meraki" {
+  meraki_debug = "true"
+}
+
+resource "meraki_networks_appliance_content_filtering" "example" {
+
+  allowed_url_patterns   = ["http://www.example.org", "http://help.com.au"]
+  blocked_url_categories = []
+  blocked_url_patterns   = ["http://www.example.com", "http://www.betting.com"]
+  network_id             = "L_828099381482771185"
+  url_category_list_size = "fullList"
+}
+
+output "meraki_networks_appliance_content_filtering_example" {
+  value = meraki_networks_appliance_content_filtering.example
+}

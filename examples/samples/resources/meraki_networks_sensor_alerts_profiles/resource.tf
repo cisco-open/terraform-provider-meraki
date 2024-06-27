9fd49313599a5d7490f20afb87343d3987146b62
Francisco Muñoz <fmunoz@cloverhound.com>
2024-06-27 10:05:57 -0600

    resource meraki_networks_appliance_firewall_l3_firewall_rules this_rule - rules issues for empty ruleset and syslog_default_rule #76

---
+++ examples/samples/resources/meraki_networks_sensor_alerts_profiles/resource.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    meraki = {
+      version = "0.2.4-alpha"
+      source  = "hashicorp.com/edu/meraki"
+      # "hashicorp.com/edu/meraki" is the local built source, change to "cisco-en-programmability/meraki" to use downloaded version from registry
+    }
+  }
+}
+
+provider "meraki" {
+  meraki_debug = "true"
+}
+
+resource "meraki_networks_sensor_alerts_profiles" "this" {
+
+  conditions = [
+    {
+      metric = "temperature"
+      duration = 60
+      threshold = {
+        temperature = {
+          quality = "good"
+        }
+      },
+    },
+    {
+      metric = "humidity"
+      duration = 60
+      threshold = {
+        humidity = {
+          quality = "good"
+        }
+      },
+    },
+    {
+      metric = "water"
+      threshold = {
+        water = {
+          present = true
+        }
+      }
+    }
+  ]
+  name       = "Sensor Alerts2"
+  network_id = "L_828099381482775342"
+  recipients = {
+    http_server_ids = ["aHR0cHM6Ly8xLjIuMy40"]
+  }
+  # serials = ["Q2FV-VYGH-ZVB3", "QBSA-D8CD-5LR6"]
+}
+
+

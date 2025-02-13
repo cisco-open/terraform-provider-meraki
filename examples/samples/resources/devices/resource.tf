2fa294dfd067e0c4a3dc653b2e61b0b1ccc71b4b
Francisco Muñoz <fmunoz@cloverhound.com>
2025-02-13 12:29:31 -0600

    1.0.2-beta

--- examples/samples/resources/devices/resource.tf
+++ examples/samples/resources/devices/resource.tf
@@ -9,7 +9,7 @@
 terraform {
   required_providers {
     meraki = {
-      version = "1.0.1-beta"
+      version = "1.0.2-beta"
       source  = "hashicorp.com/edu/meraki"
       # "hashicorp.com/edu/meraki" is the local built source, change to "cisco-en-programmability/meraki" to use downloaded version from registry
     }
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	CUSTOM_USER_AGENT  = "MerakiTerraform/1.0.1-beta Cisco"
+	CUSTOM_USER_AGENT  = "MerakiTerraform/1.0.2-beta Cisco"
 	DEFAULT_USER_AGENT = "Meraki"
 )
 

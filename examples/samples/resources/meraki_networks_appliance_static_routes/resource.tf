e48db8909df4211fa5c0f3a24d8b51078da8a42e
Francisco Muñoz <fmunoz@cloverhound.com>
2025-07-14 13:08:28 -0600

    Update import command and network_id in example configurations
    - Modified the import command in import.sh to only include "network_id".
    - Updated the network_id in resource.tf to use "meraki_networks" for consistency in example configurations.

--- examples/samples/resources/meraki_networks_appliance_static_routes/resource.tf
+++ examples/samples/resources/meraki_networks_appliance_static_routes/resource.tf
@@ -12,7 +12,7 @@ resource "meraki_networks_appliance_static_routes" "example" {
   provider   = meraki
   gateway_ip = "1.2.3.5"
   name       = "My route"
-  network_id = "L_828099381482771185"
+  network_id = "meraki_networks"
   subnet     = "192.168.1.0/24"
 }
 
